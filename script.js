99f9e8479902f3b5ab61e7bc7ed2f86002a3bf3c
henoknets <henoknetsanet98@gmail.com>
2026-07-14 14:30:07 +0300

    add reset, decrease buttons and their functionality

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const button = document.querySelector ('button');
-
-
+const button = document.querySelector ('#count');
+const reset = document.querySelector ('#reset');
+const decrease = document.querySelector ('#decrease');
 
 button.addEventListener('click', () => {
     button.textContent++;
 });
+
+reset.addEventListener('click', () => {
+    button.textContent = 0;
+});
+
+decrease.addEventListener ('click', () => {
+    if (parseInt (button.textContent) > 0) {
+        button.textContent--;
+    }
+
+});
+
